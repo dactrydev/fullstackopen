4fe26c7eb27781fac32f62a604708e907cad7bf5
Dactry <mdactry@gmail.com>
2026-05-29 09:38:12 +0300

    finished 3.1-3.8

---
+++ part3/phonebook/index.js
@@ -0,0 +1,68 @@
+let data = require("./data");
+const morgan = require("morgan");
+const express = require("express");
+const app = express();
+const PORT = 3012;
+const HOST = `http://localhost:${PORT}`;
+const PERSON_API_PATH = "/api/persons";
+
+morgan.token("body", (req) => JSON.stringify(req.body));
+
+app.use(express.json()).use(morgan(":method :url :status :body"));
+
+const generateId = () => {
+	const id = Math.floor(Math.random() * 1000) + 1000;
+	return String(id);
+};
+
+app.get("/", (req, res) => {
+	res.send("<h1>Phonebook</h1>");
+});
+
+app.get(PERSON_API_PATH, (req, res) => {
+	res.json(data);
+});
+
+app.get("/info", (req, res) => {
+	const entriesCountLine = `<p>Phonebook has info for ${data.length} people</p>`;
+	const date = Date();
+	res.json(entriesCountLine + date);
+});
+
+app.get(PERSON_API_PATH + "/:id", (req, res) => {
+	const person = data.find((d) => d.id === req.params.id);
+	if (!person) return res.status(404).end();
+	res.send(person);
+});
+
+app.delete(PERSON_API_PATH + "/:id", (req, res) => {
+	const person = data.find((d) => d.id === req.params.id);
+	if (!person) return res.status(404).end();
+
+	data = data.filter((d) => d.id !== req.params.id);
+	res.status(200).json(data);
+});
+
+app.post(PERSON_API_PATH, (req, res) => {
+	const { name, number } = req.body;
+	if (!name || !number) return res.status(400).send("content missing");
+
+	if (data.find((d) => d.name === name)) {
+		return res.status(400).send("name exists");
+	}
+	const person = {
+		id: generateId(),
+		name,
+		number,
+	};
+	data = data.concat(person);
+	res.json(data);
+});
+
+const unknownEndpoint = (req, res) => {
+	res.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+app.listen(PORT, () => console.log(`start server on :${HOST}${PERSON_API_PATH}`));
