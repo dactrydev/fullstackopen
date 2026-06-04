55a53f1863709eb2a72c4fc31454643e7d5f7e20
Dactry <mdactry@gmail.com>
2026-06-04 10:00:39 +0300

    uibuild

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,4 +1,5 @@
-let data = require("./data");
+require("dotenv").config();
+let Data = require("./models/person");
 const morgan = require("morgan");
 const express = require("express");
 const app = express();
@@ -6,65 +7,71 @@ const PORT = process.env.PORT || 3012;
 const HOST = `http://localhost:${PORT}`;
 const PERSON_API_PATH = "/api/persons";
 
-morgan.token("body", (req) => JSON.stringify(req.body));
+morgan.token("body", (req) => JSON.stringify(req.body) || " ");
 
 app.use(express.json()).use(morgan(":method :url :status :body")).use(express.static("dist"));
-
-const generateId = () => {
-	const id = Math.floor(Math.random() * 1000) + 1000;
-	return String(id);
-};
 
 app.get("/", (req, res) => {
 	res.send("<h1>Phonebook</h1>");
 });
 
-app.get(PERSON_API_PATH, (req, res) => {
-	res.json(data);
+app.get("/info", (req, res, next) => {
+	Data.find({})
+		.then((data) => {
+			const entriesCountLine = `Phonebook has info for ${data.length} people`;
+			const date = Date();
+			res.json(entriesCountLine + date);
+		})
+		.catch(next);
 });
 
-app.get("/info", (req, res) => {
-	const entriesCountLine = `<p>Phonebook has info for ${data.length} people</p>`;
-	const date = Date();
-	res.json(entriesCountLine + date);
+app.get(PERSON_API_PATH, (req, res, next) => {
+	Data.find({})
+		.then((people) => res.status(200).json(people))
+		.catch(next);
 });
 
-app.get(PERSON_API_PATH + "/:id", (req, res) => {
-	const person = data.find((d) => d.id === req.params.id);
-	if (!person) return res.status(404).end();
-	res.json(person);
+app.get(PERSON_API_PATH + "/:id", (req, res, next) => {
+	const person = Data.findById(req.params.id)
+		.then((person) => {
+			if (!person) return res.status(404).end();
+			res.json(person);
+		})
+		.catch(next);
 });
 
-app.delete(PERSON_API_PATH + "/:id", (req, res) => {
-	const person = data.find((d) => d.id === req.params.id);
-	if (!person) return res.status(404).end();
-
-	data = data.filter((d) => d.id !== req.params.id);
-	res.status(204).end();
+app.delete(PERSON_API_PATH + "/:id", (req, res, next) => {
+	Data.findByIdAndDelete(req.params.id)
+		.then((result) => {
+			res.status(204).end();
+		})
+		.catch(next);
 });
 
-app.put(PERSON_API_PATH + "/:id", (req, res) => {
-	const person = data.find((d) => d.id === req.params.id);
-	if (!person) return res.status(404).end();
-	const personObj = { ...req.body, id: req.params.id };
-	data = data.map((d) => (d.id === req.params.id ? personObj : d));
-	res.status(200).json(personObj);
+app.put(PERSON_API_PATH + "/:id", (req, res, next) => {
+	const { name, number } = req.body;
+	Data.findById(req.params.id)
+		.then((person) => {
+			if (!person) return res.status(404).end();
+			person.name = name;
+			person.number = number;
+			return person.save().then((updatedPerson) => res.json(updatedPerson));
+		})
+		.catch(next);
 });
 
-app.post(PERSON_API_PATH, (req, res) => {
+app.post(PERSON_API_PATH, (req, res, next) => {
 	const { name, number } = req.body;
 	if (!name || !number) return res.status(400).send("content missing");
 
-	if (data.find((d) => d.name === name)) {
-		return res.status(400).send("name exists");
-	}
-	const person = {
-		id: generateId(),
+	const person = new Data({
 		name,
 		number,
-	};
-	data = data.concat(person);
-	res.json(person);
+	});
+	person
+		.save()
+		.then((newPerson) => res.json(newPerson))
+		.catch(next);
 });
 
 const unknownEndpoint = (req, res) => {
@@ -73,4 +80,16 @@ const unknownEndpoint = (req, res) => {
 
 app.use(unknownEndpoint);
 
-app.listen(PORT, () => console.log(`start server on :${HOST}${PERSON_API_PATH}`));
+const errorHandler = (error, req, res, next) => {
+	console.error("Error Name: ", error.name);
+	if (error.name === "CastError") {
+		return res.status(400).send({ error: "malformatted id" });
+	}
+	if (error.name === "ValidationError") {
+		return res.status(400).json({ error: error.message });
+	}
+	next(error);
+};
+
+app.use(errorHandler);
+app.listen(PORT, () => console.log("Start server on :", HOST + PERSON_API_PATH));
---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+	console.log("give password as argument");
+	process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv[3];
+const phone = process.argv[4];
+
+const url = `mongodb+srv://dactry_phonebook:${password}@cluster0.kiwh3jl.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url, { family: 4 });
+
+const personSchema = new mongoose.Schema({
+	name: String,
+	phone: String,
+});
+
+personSchema.set("toJSON", {
+	transform: (document, returnedObject) => {
+		((returnedObject.id = returnedObject._id.toString()), delete returnedObject._id);
+		delete returnedObject.__v;
+	},
+});
+const Person = mongoose.model("Person", personSchema);
+
+if (!phone) {
+	const output = ["Phonebook: "];
+	Person.find({}).then((res) => {
+		res.forEach((r) => {
+			output.push(`${r.name} ${r.phone}`);
+		});
+		console.log(output.join("\n"));
+		mongoose.connection.close();
+	});
+	return;
+}
+
+const person = new Person({ name, phone });
+
+person.save().then((result) => {
+	console.log(`added ${result.name} number ${result.phone} to phonebook`);
+	mongoose.connection.close();
+});
